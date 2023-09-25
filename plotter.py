fbad9a39e4f40b8962934cfedfcd4b0a303700b1
Àlex Romero-Casañas <aromero@immunostep.com>
2023-09-25 09:56:35 +0200

    Added route to Desktop as default place to look for files

--- plotter.py
+++ plotter.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import sys
 
 ftp = input("File to plot: ")
+route = "/mnt/c/Users/Laboratorio/Desktop/"
 
-df = pd.read_csv(ftp + ".csv", sep=";", header=2)
+df = pd.read_csv("/mnt/c/Users/Laboratorio/Desktop/" + ftp + ".csv", sep=";", header=2)
 
 plt.plot(df["ml"], df["mAU"], color="#0073b8")
 plt.title(ftp)
