54e0b4a7fd0acf69b715e82daffa6d84c9124112
Àlex Romero-Casañas <aromero@immunostep.com>
2023-11-16 13:58:56 +0100

    Had uncommited changes

--- plotter.py
+++ plotter.py
@@ -6,7 +6,7 @@ ftp = input("File to plot: ")
 route = "/mnt/c/Users/Laboratorio/Desktop/"
 
 df = pd.read_csv("/mnt/c/Users/Laboratorio/Desktop/" + ftp + ".csv", sep=";", header=2)
-
+print(df)
 plt.plot(df["ml"], df["mAU"], color="#0073b8")
 plt.title(ftp)
 plt.xlabel("ml")
