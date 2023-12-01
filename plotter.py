9e83cec46cb6613f88b69b48e848bacaa7b626af
Àlex Romero-Casañas <aromero@immunostep.com>
2023-12-01 07:59:50 +0100

    Had uncommited changes

--- plotter.py
+++ plotter.py
@@ -5,7 +5,7 @@ import sys
 ftp = input("File to plot: ")
 route = "/mnt/c/Users/Laboratorio/Desktop/"
 
-df = pd.read_csv("/mnt/c/Users/Laboratorio/Desktop/" + ftp + ".csv", sep=";", header=2)
+df = pd.read_csv("/mnt/c/Users/Laboratorio/Desktop/Cromatogramas/" + ftp + ".csv", sep=";", header=2)
 
 plt.plot(df["ml"], df["mAU"], color="#0073b8")
 plt.title(ftp)
